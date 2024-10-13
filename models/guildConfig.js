b8181e234eded715b4ec41fefcc51e8ee5e1dca2
Avexiis <samelykimmel@gmail.com>
2024-10-13 05:02:31 -0400

    final additions

--- models/guildConfig.js
+++ models/guildConfig.js
@@ -1,10 +1,12 @@
+// models/guildConfig.js
+
 const mongoose = require('mongoose');
 
 const guildConfigSchema = new mongoose.Schema({
     guildId: { type: String, required: true, unique: true },
-    verifiedRoleId: { type: String, required: true },
+    verifiedRoleIds: { type: [String], required: true }, // Changed to an array of role IDs
     panelMessage: { type: String, required: true },
     timer: { type: Number, required: true },
-});
+}, { collection: 'GatekeeperGuildConfigs' });
 
 module.exports = mongoose.model('GuildConfig', guildConfigSchema);
